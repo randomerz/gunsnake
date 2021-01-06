18d5912cd8397702ddb0dd047085cb6a1fd45555
boomo <randomerz56@gmail.com>
2021-01-05 21:46:13 -0500

    added game timer and player movement

---
+++ gunsnake/Assets/Scripts/Systems/GameHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+        TimeTickSystem.Create();
+
+        //TimeTickSystem.OnTick += delegate (object sender, TimeTickSystem.OnTickEventArgs e)
+        //{
+        //    Debug.Log("tick: " + TimeTickSystem.GetTick());
+        //};
+        //TimeTickSystem.OnTick_4 += delegate (object sender, TimeTickSystem.OnTickEventArgs e)
+        //{
+        //    Debug.Log("QUAD");
+        //};
+    }
+    
+    void Update()
+    {
+        
+    }
+}
