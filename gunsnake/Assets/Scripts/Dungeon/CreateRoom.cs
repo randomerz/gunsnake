2bd794896a8cbbeb234becdea53ddc5d9c838091
Daniel Carr <randomerz56@gmail.com>
2021-02-07 02:20:57 -0500

    Added room editor
    - added room scriptable object
    - added custom editor and scene to make rooms (room2obj)
    - can create new objects, save from tiles, load tiles
    - imported new recording package

---
+++ gunsnake/Assets/Scripts/Dungeon/CreateRoom.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.Tilemaps;
+
+public class CreateRoom : MonoBehaviour
+{
+    public const int MAX_ROOM_SIZE = 100;
+
+    public string roomName;
+    public Room.RoomType roomType;
+
+    public bool hasNorthDoor;
+    public bool hasEastDoor;
+    public bool hasSouthDoor;
+    public bool hasWestDoor;
+
+    public Room currentRoom;
+
+
+    public Tilemap floor;
+    public Tilemap sideWall;
+    public Tilemap topWall;
+    public TileBase defaultFloor;
+    public TileBase defaultSideWall;
+    public TileBase defaultTopWall;
+
+
+    public void String2Room(string roomString, int width, int height)
+    {
+        char[] roomChar = roomString.ToCharArray();
+
+        string s = "";
+        ClearTilemaps();
+        for (int r = 0; r < height; r++)
+        {
+            for (int c = 0; c < width; c++)
+            {
+                char t = roomChar[r * (width + 1) + c];
+
+                if (t == Room.WALLCHAR)
+                {
+                    sideWall.SetTile(new Vector3Int(c, height - r - 1, 0), defaultSideWall);
+                    topWall.SetTile(new Vector3Int(c, height - r - 1, 0), defaultTopWall);
+                }
+                if (t == Room.FLOORCHAR)
+                {
+                    floor.SetTile(new Vector3Int(c, height - r - 1, 0), defaultFloor);
+                }
+                s += t;
+            }
+            s += '\n';
+        }
+
+        Debug.Log(s);
+    }
+
+    //public void String2Room(string roomString)
+    //{
+    //    ClearTilemaps();
+    //}
+
+    public void String2Room()
+    {
+        String2Room(currentRoom.roomString, currentRoom.width, currentRoom.height);
+    }
+
+    public string Room2String()
+    {
+        string roomString = "";
+
+        int width = 0;
+        int height = 0;
+
+        for (int r = 0; r < MAX_ROOM_SIZE; r++)
+        {
+            for (int c = 0; c < MAX_ROOM_SIZE; c++)
+            {
+                Vector3Int pos = new Vector3Int(c, MAX_ROOM_SIZE - r - 1, 0);
+                TileBase sideTile = sideWall.GetTile(pos);
+                TileBase topTile = topWall.GetTile(pos);
+                TileBase floorTile = floor.GetTile(pos);
+
+                if (sideTile != null || topTile != null || floorTile != null)
+                {
+                    width = Mathf.Max(width, c);
+                    height = Mathf.Max(height, MAX_ROOM_SIZE - r - 1);
+                }
+            }
+        }
+
+        width++;
+        height++;
+
+        for (int r = 0; r < height; r++)
+        {
+            for (int c = 0; c < width; c++)
+            {
+                Vector3Int pos = new Vector3Int(c, height - r - 1, 0);
+                TileBase sideTile = sideWall.GetTile(pos);
+                TileBase topTile = topWall.GetTile(pos);
+                TileBase floorTile = floor.GetTile(pos);
+
+                if (sideTile != null || topTile != null)
+                {
+                    roomString += Room.WALLCHAR;
+                    sideWall.SetTile(pos, defaultSideWall);
+                    topWall.SetTile(pos, defaultTopWall);
+                }
+                else if (floorTile != null)
+                {
+                    roomString += Room.FLOORCHAR;
+                    floor.SetTile(pos, defaultFloor);
+                }
+                else
+                {
+                    roomString += Room.EMPTYCHAR;
+                }
+            }
+            roomString += '\n';
+        }
+
+        Debug.Log("Saved a room of width " + width + " and height " + height + "!");
+        currentRoom.roomString = roomString;
+        currentRoom.width = width;
+        currentRoom.height = height;
+        return roomString;
+    }
+
+
+    public void AddTops()
+    {
+        for (int r = 0; r < MAX_ROOM_SIZE; r++)
+        {
+            for (int c = 0; c < MAX_ROOM_SIZE; c++)
+            {
+                Vector3Int pos = new Vector3Int(c, MAX_ROOM_SIZE - r - 1, 0);
+                TileBase sideTile = sideWall.GetTile(pos);
+                TileBase topTile = topWall.GetTile(pos);
+
+                if (sideTile != null || topTile != null)
+                {
+                    sideWall.SetTile(pos, defaultSideWall);
+                    topWall.SetTile(pos, defaultTopWall);
+                    floor.SetTile(pos, null);
+                }
+            }
+        }
+    }
+
+    private void ClearTilemaps()
+    {
+        floor.ClearAllTiles();
+        sideWall.ClearAllTiles();
+        topWall.ClearAllTiles();
+    }
+
+
+}
---
+++ gunsnake/Assets/Scripts/Editor/CreateRoomEditor.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.EditorTools;
+
+[CustomEditor(typeof(CreateRoom))]
+public class CreateRoomEditor : Editor
+{
+    protected static bool showReferences;
+    
+    private CreateRoom _target;
+    private const string objPath = "Assets/Scriptable Objects/Rooms/";
+
+    private void OnEnable()
+    {
+        _target = (CreateRoom)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("roomName"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("roomType"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("hasNorthDoor"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("hasEastDoor"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("hasSouthDoor"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("hasWestDoor"));
+
+
+        if (GUILayout.Button("Add Tops"))
+        {
+            _target.AddTops();
+        }
+        if (GUILayout.Button("Create New Room from Scene!"))
+        {
+            Room newRoom = ScriptableObject.CreateInstance<Room>();
+
+            string roomName = serializedObject.FindProperty("roomName").stringValue;
+            AssetDatabase.CreateAsset(newRoom, objPath + roomName + ".asset");
+            //AssetDatabase.CreateAsset(newRoom, objPath + "tempRoom.asset");
+            AssetDatabase.SaveAssets();
+            EditorGUIUtility.PingObject(newRoom);
+
+            serializedObject.FindProperty("currentRoom").objectReferenceValue = newRoom;
+            serializedObject.ApplyModifiedProperties();
+
+            SaveData();
+        }
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("currentRoom"));
+
+        if (GUILayout.Button("Load Current Room Data"))
+        {
+            LoadData();
+        }
+        if (GUILayout.Button("Save Data into Current Room"))
+        {
+            SaveData();
+        }
+
+
+        // fold out
+        showReferences = EditorGUILayout.Foldout(showReferences, "References");
+
+        if (showReferences)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("floor"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("sideWall"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("topWall"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("defaultFloor"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("defaultSideWall"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("defaultTopWall"));
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void LoadData()
+    {
+        SerializedProperty prop = serializedObject.FindProperty("currentRoom");
+        SerializedObject propObj = new SerializedObject(prop.objectReferenceValue);
+
+        serializedObject.FindProperty("roomName").stringValue = propObj.FindProperty("roomName").stringValue;
+        serializedObject.FindProperty("roomType").enumValueIndex = propObj.FindProperty("roomType").enumValueIndex;
+        serializedObject.FindProperty("hasNorthDoor").boolValue = propObj.FindProperty("hasNorthDoor").boolValue;
+        serializedObject.FindProperty("hasEastDoor").boolValue = propObj.FindProperty("hasEastDoor").boolValue;
+        serializedObject.FindProperty("hasSouthDoor").boolValue = propObj.FindProperty("hasSouthDoor").boolValue;
+        serializedObject.FindProperty("hasWestDoor").boolValue = propObj.FindProperty("hasWestDoor").boolValue;
+
+        // loading into scene stuff
+        _target.String2Room();
+
+    }
+
+    private void SaveData()
+    {
+        SerializedProperty prop = serializedObject.FindProperty("currentRoom");
+        SerializedObject propObj = new SerializedObject(prop.objectReferenceValue);
+
+        propObj.FindProperty("roomName").stringValue = serializedObject.FindProperty("roomName").stringValue;
+        propObj.FindProperty("roomType").enumValueIndex = serializedObject.FindProperty("roomType").enumValueIndex;
+        propObj.FindProperty("hasNorthDoor").boolValue = serializedObject.FindProperty("hasNorthDoor").boolValue;
+        propObj.FindProperty("hasEastDoor").boolValue = serializedObject.FindProperty("hasEastDoor").boolValue;
+        propObj.FindProperty("hasSouthDoor").boolValue = serializedObject.FindProperty("hasSouthDoor").boolValue;
+        propObj.FindProperty("hasWestDoor").boolValue = serializedObject.FindProperty("hasWestDoor").boolValue;
+
+        propObj.ApplyModifiedProperties();
+
+        _target.Room2String();
+    }
+}
