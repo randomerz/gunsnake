138eac896572ede6845ad972284ea32a1e78a1d5
Orange232 <62818571+Orange232@users.noreply.github.com>
2021-02-21 18:24:10 -0500

    Started the coding

--- gunsnake/Assets/Scripts/Items/Artifacts/Artifact.cs
+++ gunsnake/Assets/Scripts/Items/Artifacts/Artifact.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Artifact : MonoBehaviour
 {
+    private GameObject player;
+    private int artifact_code;
+    private int artifact_val;
+    public Artifact(GameObject p)
+    {
+        player = p;
+    }
+    public Artifact(GameObject p, int an, int av)
+    {
+        player = p;
+        artifact_code = an;
+        artifact_val = av;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,13 @@ public class Artifact : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.GameObject == player)
+        {
+            
+        }
     }
 }
