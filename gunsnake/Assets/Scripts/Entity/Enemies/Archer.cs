01f7b4ea7925d29774600124408e5164d74d26d7
Allison Lu <allison.lu@gatech.edu>
2021-03-20 10:02:28 -0400

    set up enemies

---
+++ gunsnake/Assets/Scripts/Entity/Enemies/Archer.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Enemy
+{
+    [Header("Archer")]
+    [Tooltip("1 = 4 game ticks")]
+    public int attackSpeed;
+    private int ticksTillAttack;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (randomizeStartingVars)
+            ticksTillAttack = Random.Range(1, attackSpeed);
+        else
+            ticksTillAttack = attackSpeed;
+    }
+
+    public override void EnemyTick(int tick)
+    {
+        if (tick % 4 == 0)
+        {
+            ticksTillAttack -= 1;
+
+            // visuals
+            if (animator != null)
+                SetAnimatorBool("isIdle", true);
+
+            switch (ticksTillAttack)
+            {
+                case 2:
+                case 1:
+                    if (animator != null)
+                        SetAnimatorBool("isPrep", true);
+                    break;
+                default:
+                    if (ticksTillAttack <= 0)
+                    {
+                        ticksTillAttack = attackSpeed;
+
+                        // visuals
+                        if (animator != null)
+                            SetAnimatorBool("isAttack", true);
+
+
+                        // attack if within range
+                        GameObject closestSeg = GetClosestPlayerSegment();
+                        if ((closestSeg.transform.position - transform.position).magnitude <= 1)
+                        {
+                            Attack(closestSeg);
+                        }
+                        // else move closer
+                        else
+                        {
+                            Move(GetDirectionToPlayer(false));
+                        }
+                    }
+                    break;
+            }
+
+        }
+
+        // other updates not %4 tick
+        animator.UpdatePosition();
+    }
+
+    // TODO: fix movement, see card. 
+    // May need new to create new method, GetDirectionsToPlayer(shouldDiag)
+    private void Move(Vector3 dir)
+    {
+        animator.SetOrigPos(transform.position);
+        if (dir.x > 0)
+            animator.SetFacing(false);
+        else if (dir.x < 0)
+            animator.SetFacing(true);
+
+        if (CanMove(transform.position + dir))
+            transform.position += dir;
+    }
+
+    private void Attack(GameObject seg)
+    {
+        PlayerSegmentHealth h = seg.GetComponent<PlayerSegmentHealth>();
+        if (h != null)
+        {
+            h.TakeDamage(damage);
+        }
+    }
+}
